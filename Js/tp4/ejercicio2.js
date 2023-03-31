97217075f6aef865105e2c7c2e1dd8942eb38f42
Maoi11 <marisaindianagarat@gmail.com>
2023-03-31 17:31:39 -0300

    ejercicio2 completo

--- Js/tp4/ejercicio2.js
+++ Js/tp4/ejercicio2.js
@@ -6,3 +6,37 @@
 // Un método informar() que retorne la información del estado de la cuenta. 
 
 // Utiliza este objeto para mostrar la descripción, ingresar y extraer dinero y volver a mostrar la descripción del estado de la cuenta.
+
+class Cuenta {
+    constructor(titular) {
+        this.titular = titular;
+        this.saldo = 0;
+    }
+
+    ingresar(dinero) {
+        if (!isNaN(dinero)) {
+            this.saldo += dinero;
+            document.write(`<br>Usted ingresó $${dinero} a su cuenta.`);
+        }
+    }
+
+    extraer(monto) {
+        if (!isNaN(monto)) {
+            this.saldo -= monto;
+            document.write(`<br>Usted extrajo $${monto} de su cuenta.`);
+        } 
+    }
+
+    informar() {
+        document.write(`<br>Usted tiene disponibles $${this.saldo} en su cuenta.`);
+    }
+
+
+}
+
+let cuenta = new Cuenta('Alex');
+cuenta.ingresar(3500);
+cuenta.extraer(500);
+cuenta.informar();
+
+
